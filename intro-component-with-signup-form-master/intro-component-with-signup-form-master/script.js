76751a3547ebbbe411d80e7548c1993fb25c1802
fakhrirf <fakhririyadh@gmail.com>
2020-11-03 04:11:51 +0700

    add files

---
+++ intro-component-with-signup-form-master/intro-component-with-signup-form-master/script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const firstName = form['firstname'].value;
+    const lastName = form['lastname'].value;
+    const email = form['email'].value;
+    const password = form['password'].value;
+    
+
+
+
+    if(firstName === ''){
+        addErrorTo('firstname', 'First Name is required');    
+    } else {
+        removeErrorFrom('firstname');
+    }
+
+    if(lastName === ''){
+        addErrorTo('lastname', 'last Name is required');
+    } 
+
+    if (email === ''){
+        addErrorTo('email', 'Email is required');
+    } else if (!isValid(email)){
+        addErrorTo('email', 'Email is not valid');
+    } else{
+        removeErrorFrom('email');
+    }
+
+    if(password === ''){
+        addErrorTo('password', 'Password is required');
+    }
+
+    
+});
+
+
+function addErrorTo(field, message) {
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    small.style.opacity = '1';
+}
+
+
+function removeErrorFrom(field) {
+    const formControl = form[field].parentNode;
+    formControl.classList.remove('error');
+    
+    const small = formControl.querySelector('small');
+    small.style.opacity = '0';
+    
+}
+
+function isValid(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
